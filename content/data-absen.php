1faf58bc8d666a23626f1efa54d63c268ab7937f
maulidah-fitna <maulidah.fitri37@smk.belajar.id>
2025-02-18 00:30:22 +0700

    mengupdate data
    mengupdate data absen

--- content/data-absen.php
+++ content/data-absen.php
@@ -2,7 +2,7 @@
 if(!defined('INDEX')) die("");
 ?>
 
-<head>
+<!-- <head>
     <link rel="stylesheet" href="absen-style.css">
 </head>
 <body>
@@ -13,30 +13,209 @@ if(!defined('INDEX')) die("");
             <div class="class-buttons">
                 <span class="labels">Putra</span>
                 <div class="class-buttons">
-                    <a href="?hal=xpplgpa"><button>X PPLG</button></a>
-                    <a href="?hal=xaklpa"><button>X AKL</button></a>
-                    <a href="?hal=xirplpa"><button>XI RPL</button></a>
-                    <a href="?hal=xilpspa"><button>XI LPS</button></a>
-                    <a href="?hal=xiirplpa"><button>XII RPL</button></a>
-                    <a href="?hal=xiipbspa"><button>XII PBS</button></a>
+                    <a href="?hal=presensi&k=10&j=pplg&g=l"><button>X PPLG</button></a>
+                    <a href="?hal=presensi&k=10&j=akl&g=l"><button>X AKL</button></a>
+                    <a href="?hal=presensi&k=11&j=rpl&g=l"><button>XI RPL</button></a>
+                    <a href="?hal=presensi&k=11&j=lps&g=l"><button>XI LPS</button></a>
+                    <a href="?hal=presensi&k=12&j=rpl&g=l"><button>XII RPL</button></a>
+                    <a href="?hal=presensi&k=12&j=pbs&g=l"><button>XII PBS</button></a>
                 </div>
             </div>
             <div class="class-buttons">
                 <span class="labels">Putri</span>
                 <div class="class-buttons">
-                    <a href="?hal=xpplgpi"><button>X PPLG</button></a>
-                    <a href="?hal=xaklpi"><button>X AKL</button></a>
-                    <a href="?hal=xirplpi"><button>XI RPL</button></a>
-                    <a href="?hal=xilpspi"><button>XI LPS</button></a>
-                    <a href="?hal=xiirplpi"><button>XII RPL</button></a>
-                    <a href="?hal=xiipbspi"><button>XII PBS</button></a>
-                    <a href="?hal=presensi&k=10&j=pplg&g=l"><button>X PPLG PUTRA</button></a>
+                    <a href="?hal=presensi&k=10&j=pplg&g=p"><button>X PPLG</button></a>
+                    <a href="?hal=presensi&k=10&j=akl&g=p"><button>X AKL</button></a>
+                    <a href="?hal=presensi&k=11&j=rpl&g=p"><button>XI RPL</button></a>
+                    <a href="?hal=presensi&k=11&j=lps&g=p"><button>XI LPS</button></a>
+                    <a href="?hal=presensi&k=12&j=rpl&g=p"><button>XII RPL</button></a>
+                    <a href="?hal=presensi&k=12&j=pbs&g=p"><button>XII PBS</button></a>
                 </div>
             </div>
         </div>
     </div>
 
-</body>
+</body> -->
+
+<div class="row">
+        <div class="col-lg-6 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+            <div class="inner">
+
+              <a href="?hal=presensi&k=10&j=pplg&g=l"><h3>X PPLG</h3></a>
+
+            </div>
+            <div class="icon">
+              <!-- <i class="ion ion-bag"></i> -->
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <div class="col-lg-6 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+            <div class="inner">
+
+              <a href="?hal=presensi&k=10&j=akl&g=l"><h3>X AKL</h3></a>
+
+            </div>
+            <div class="icon">
+              <!-- <i class="ion ion-bag"></i> -->
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <div class="col-lg-6 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+            <div class="inner">
+
+              <a href="?hal=presensi&k=11&j=rpl&g=l"><h3>XI RPL</h3></a>
+
+            </div>
+            <div class="icon">
+              <!-- <i class="ion ion-bag"></i> -->
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <div class="col-lg-6 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+            <div class="inner">
+
+              <a href="?hal=presensi&k=11&j=lps&g=l"><h3>XI LPS</h3></a>
+
+            </div>
+            <div class="icon">
+              <!-- <i class="ion ion-bag"></i> -->
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <div class="col-lg-6 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+            <div class="inner">
+
+              <a href="?hal=presensi&k=12&j=rpl&g=l"><h3>XII RPL</h3></a>
+
+            </div>
+            <div class="icon">
+              <!-- <i class="ion ion-bag"></i> -->
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <div class="col-lg-6 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+            <div class="inner">
+
+              <a href="?hal=presensi&k=12&j=pbs&g=l"><h3>XII PBS</h3></a>
+
+            </div>
+            <div class="icon">
+              <!-- <i class="ion ion-bag"></i> -->
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+       
+        <!-- ./col -->
+      </div>
+
+
+<div class="row">
+<div class="col-lg-6 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+            <div class="inner">
+
+              <a href="?hal=presensi&k=10&j=pplg&g=p"><h3>X PPLG</h3></a>
+
+            </div>
+            <div class="icon">
+              <!-- <i class="ion ion-bag"></i> -->
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <div class="col-lg-6 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+            <div class="inner">
+
+              <a href="?hal=presensi&k=10&j=akl&g=p"><h3>X AKL</h3></a>
+
+            </div>
+            <div class="icon">
+              <!-- <i class="ion ion-bag"></i> -->
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <div class="col-lg-6 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+            <div class="inner">
+
+              <a href="?hal=presensi&k=11&j=rpl&g=p"><h3>XI RPL</h3></a>
+
+            </div>
+            <div class="icon">
+              <!-- <i class="ion ion-bag"></i> -->
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <div class="col-lg-6 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+            <div class="inner">
+
+              <a href="?hal=presensi&k=11&j=lps&g=p"><h3>XI LPS</h3></a>
+
+            </div>
+            <div class="icon">
+              <!-- <i class="ion ion-bag"></i> -->
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <div class="col-lg-6 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+            <div class="inner">
+
+              <a href="?hal=presensi&k=12&j=rpl&g=p"><h3>XII RPL</h3></a>
+
+            </div>
+            <div class="icon">
+              <!-- <i class="ion ion-bag"></i> -->
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <div class="col-lg-6 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+            <div class="inner">
+
+              <a href="?hal=presensi&k=12&j=pbs&g=p"><h3>XII PBS</h3></a>
+
+            </div>
+            <div class="icon">
+              <!-- <i class="ion ion-bag"></i> -->
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+       
+        <!-- ./col -->
+      </div>
+
 </html>
 
 <!-- <div class="class-buttons">
